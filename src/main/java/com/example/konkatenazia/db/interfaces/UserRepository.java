3aec3f71e55c51a87c68bc61bd279d39ecf1aea5
kanka <daniilsahnov@mail.ru>
2023-06-05 04:46:50 +0300

    addRepo Gone

---
+++ src/main/java/com/example/konkatenazia/db/interfaces/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.konkatenazia.db.interfaces;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository {
+
+}
--- src/main/java/com/example/konkatenazia/model/entities/User.java
+++ src/main/java/com/example/konkatenazia/model/entities/User.java
@@ -3,12 +3,16 @@ package com.example.konkatenazia.model.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 public class User {
     @Id
     private String id;
